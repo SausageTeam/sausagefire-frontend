1fd8ead27a1941fe06c095060c492250f053704b
Martin Li <lixphilosophy@gmail.com>
2020-03-21 20:52:14 -0400

    finish avatar ui

--- src/app/employee/onboarding/avatar/avatar.component.ts
+++ src/app/employee/onboarding/avatar/avatar.component.ts
@@ -10,12 +10,32 @@ export class AvatarComponent implements OnInit {
 
   ifUnclockNext : boolean = false;
 
+  imageSrc : any = "assets/default-avatar.jpg";
+  avatar : any = null;
+
   constructor(
     private router: Router
   ) { }
 
   ngOnInit(): void {
   }
+
+  onFileSelected(event : any) : void {
+    if (event.target.files && event.target.files[0]) {
+
+      this.avatar = event.target.files[0];
+
+      const reader = new FileReader();
+
+      reader.readAsDataURL(this.avatar);
+
+      reader.onload = (e) => {
+        this.imageSrc = e.target.result;
+      }
+
+    }  
+  }
+    
 
   onSaveClick() : void {
     this.ifUnclockNext = true;

2c14e23f0243e09116979dcb7cf5e4acd37d077f
Martin Li <lixphilosophy@gmail.com>
2020-03-22 11:39:56 -0400

    finish driving ui

--- src/app/employee/onboarding/driving/driving.component.ts
+++ src/app/employee/onboarding/driving/driving.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { OnboardingDriving } from 'src/app/domain/onboardingDriving.module';
 
 @Component({
   selector: 'app-driving',
@@ -8,7 +9,37 @@ import { Router } from '@angular/router';
 })
 export class DrivingComponent implements OnInit {
 
+  onboardingDriving : OnboardingDriving = new OnboardingDriving();
+
   ifUnclockNext : boolean = false;
+
+  driverLisenseOptionsIndex : number = 1;
+  driverLisenseOptions = [
+    "Yes",
+    "No"
+  ];
+
+  carOptionsIndex : number = 1;
+  carOptions = [
+    "Yes",
+    "No"
+  ];
+
+  driverLicense : string = "";
+  driverLicenseExpirationDate : string = "";
+  driverLicenseDoc : any = null;
+
+  ifDriverLicense : boolean = true;
+  ifDriverLicenseExpirationDate : boolean = true;
+  ifDriverLienseDoc : boolean = true;
+
+  maker : string = "";
+  model : string = "";
+  color : string = "";
+
+  ifMakerEnter : boolean = true;
+  ifModelEnter : boolean = true;
+  ifColorEnter : boolean = true;
 
   constructor(
     private router: Router
@@ -17,12 +48,44 @@ export class DrivingComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  inputCheck() : void {
+  selectDriverLisenseOption(i : number) {
+    if(this.driverLisenseOptionsIndex !== i && this.driverLisenseOptionsIndex === 1) {
+      this.ifUnclockNext = false;
+    }
+    this.driverLisenseOptionsIndex = i;
+  }
 
+  selectCarOption(i : number) {
+    if(this.carOptionsIndex !== i && this.carOptionsIndex === 1) {
+      this.ifUnclockNext = false;
+    }
+    this.carOptionsIndex = i;
+  }
+
+  inputCheck() : void {
+    if(this.driverLisenseOptionsIndex === 0) {
+      if(!this.driverLicense || this.driverLicense === '') this.ifDriverLicense = false;
+      if(!this.driverLicenseExpirationDate || this.driverLicenseExpirationDate === '') this.ifDriverLicenseExpirationDate = false;
+    } 
+    if(this.carOptionsIndex === 0) {
+      if(!this.maker || this.maker === '') this.ifMakerEnter = false;
+      if(!this.model || this.model === '') this.ifModelEnter = false;
+      if(!this.color || this.color === '') this.ifColorEnter = false;
+    }
   }
 
   nextCheck() : boolean {
-    return true;
+    const test = (field : string) => field && field !== '';
+
+    if(this.driverLisenseOptionsIndex == 1 && this.carOptionsIndex === 1) {
+      return true;
+    }
+
+    if(this.driverLisenseOptionsIndex == 0) {
+      return [this.driverLicense, this.driverLicenseExpirationDate].every(test);
+    }
+
+    return [this.maker, this.model, this.color].every(test);
   }
 
   onSaveClick() : void {
@@ -31,7 +94,6 @@ export class DrivingComponent implements OnInit {
 
     if(this.nextCheck()) {
       this.ifUnclockNext = true;
-
     }
   }
 
@@ -41,5 +103,54 @@ export class DrivingComponent implements OnInit {
 
   onNextClick() : void {
     this.router.navigate(['/employee/onboarding/reference']);
+  }
+
+  onMakerEdit(event : any) {
+    if (!this.maker || this.maker === '') {
+      this.ifMakerEnter = false;
+      this.ifUnclockNext = false;
+    } else {
+      this.ifMakerEnter = true;
+    }
+  }
+
+  onModelEdit(event: any) {
+    if (!this.model || this.model === '') {
+      this.ifModelEnter = false;
+      this.ifUnclockNext = false;
+    } else {
+      this.ifModelEnter = true;
+    }
+  }
+
+  onColorEdit(event: any) {
+    if (!this.color || this.color === '') {
+      this.ifColorEnter = false;
+      this.ifUnclockNext = false;
+    } else {
+      this.ifColorEnter = true;
+    }
+  }
+
+  onDriverLiceseEdit(evnet: any) {
+    if (!this.driverLicense || this.driverLicense === '') {
+      this.ifDriverLicense = false;
+      this.ifUnclockNext = false;
+    } else {
+      this.ifDriverLicense = true;
+    }
+  }
+
+  onDriverLicenseExpirationDateEdit(event: any) {
+    if (!this.driverLicenseExpirationDate || this.driverLicenseExpirationDate === '') {
+      this.ifDriverLicenseExpirationDate = false;
+      this.ifUnclockNext = false;
+    } else {
+      this.ifDriverLicenseExpirationDate = true;
+    }
+  }
+
+  onDriverLicenseDocSelect(event: any) {
+
   }
 }

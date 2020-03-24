81dd32586be16243069c5f08540cb2705afed84d
Martin Li <lixphilosophy@gmail.com>
2020-03-24 07:50:59 -0400

    add app image

---
+++ src/app/app/avatar/avatar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/service/app.service';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.css']
+})
+export class AvatarComponent implements OnInit {
+
+  imageSrc : any = "assets/default-avatar.jpg";
+
+  constructor(
+    private appService : AppService
+  ) { }
+
+  ngOnInit(): void {
+    this.appService.getAvatarService().subscribe(
+      (res) => {
+        console.log(res);
+        this.imageSrc = res.appDomain.avatarUri ? res.appDomain.avatarUri : "assets/default-avatar.jpg";
+      }
+    );
+  }
+
+}
